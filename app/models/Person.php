46550e5f5f8a9fc775f8cac531d77259b40cf113
Khalid Sadat <khalid_saadat@hotmail.com>
2022-01-24 23:14:33 -0500

    Minor bug

--- app/models/Person.php
+++ app/models/Person.php
@@ -9,32 +9,32 @@ class Person extends Model{
     }
 
 	public function getAll(){
-        $stmt = self::$_connection->prepare("SELECT * FROM Person");
+        $stmt = self::$_connection->prepare("SELECT * FROM person");
         $stmt->execute();
-    	$stmt->setFetchMode(PDO::FETCH_CLASS, 'Person');
+    	$stmt->setFetchMode(PDO::FETCH_CLASS, 'person');
 		return $stmt->fetchAll();
     }
 
     public function find($person_id){
-        $stmt = self::$_connection->prepare("SELECT * FROM Person WHERE person_id = :person_id");
+        $stmt = self::$_connection->prepare("SELECT * FROM person WHERE person_id = :person_id");
         $stmt->execute(['person_id'=>$person_id]);
-        $stmt->setFetchMode(PDO::FETCH_CLASS, 'Person');
+        $stmt->setFetchMode(PDO::FETCH_CLASS, 'person');
         return $stmt->fetch();
     }
 
     public function insert(){
-	    $stmt = self::$_connection->prepare("INSERT INTO Person(first_name, last_name) VALUES(:first_name,:last_name)");
+	    $stmt = self::$_connection->prepare("INSERT INTO person(first_name, last_name) VALUES(:first_name,:last_name)");
         $stmt->execute(['first_name'=>$this->first_name,
          'last_name'=>$this->last_name]);
     }
 
     public function delete(){
-        $stmt = self::$_connection->prepare("DELETE FROM Person WHERE person_id = :person_id");
+        $stmt = self::$_connection->prepare("DELETE FROM person WHERE person_id = :person_id");
         $stmt->execute(['person_id'=>$this->person_id]);
     }
 
     public function update(){
-        $stmt = self::$_connection->prepare("UPDATE Person SET first_name = :first_name, last_name = :last_name WHERE person_id = :person_id");
+        $stmt = self::$_connection->prepare("UPDATE person SET first_name = :first_name, last_name = :last_name WHERE person_id = :person_id");
         $stmt->execute(['first_name'=>$this->first_name,
          'last_name'=>$this->last_name, 'person_id'=>$this->person_id]);
     }
